0e850826fdc11acaac4233ee5bcdbc051529d74e
Gabriele-Viola <gabrieleviola50@gmail.com>
2025-02-06 15:55:56 +0100

    update menu page

--- Front-end/src/components/AppHeader.jsx
+++ Front-end/src/components/AppHeader.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export default function AppHeader() {
     return (
-        <header className="">
+        <header className="mb-5">
             <div className="m-auto w-4/5 grid grid-cols-2">
 
                 <NavLink to={'/'} className="logo p-4 basis-1/4">logo</NavLink>
--- Front-end/src/pages/Menu.jsx
+++ Front-end/src/pages/Menu.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react"
+import { GiFlour, GiLindenLeaf } from 'react-icons/gi'
 
 export default function Menu() {
     const [menu, setMenu] = useState([])
+    const [antipasti, setAntipasti] = useState([])
+    const [primi, setPrimi] = useState([])
+    const [secondi, setSecondi] = useState([])
+    const [desserts, setDesserts] = useState([])
     const [error, setError] = useState('')
 
     const urlIndexMenu = `${import.meta.env.VITE_API_URL}/menu`
@@ -14,6 +19,11 @@ export default function Menu() {
             }
             const data = await resp.json()
             setMenu(data.data)
+            setAntipasti(data.data?.filter(piatto => piatto.tipologia_portata === 'Antipasto'))
+            setPrimi(data.data?.filter(piatto => piatto.tipologia_portata === 'Primo'))
+            setSecondi(data.data?.filter(piatto => piatto.tipologia_portata === 'Secondo'))
+            setDesserts(data.data?.filter(piatto => piatto.tipologia_portata === 'Dessert'))
+
         } catch (err) {
             setError(err.message)
         }
@@ -22,32 +32,127 @@ export default function Menu() {
     useEffect(() => {
         fetchData(urlIndexMenu)
     }, [])
-    console.log(menu);
+    console.log({ menu, antipasti, primi, secondi, desserts });
 
 
     return (
-        <div className="bodyMenu">
-            <h1>il menu propone</h1>
-            <div className="entrè">
-                <h3>antipasti</h3>
-                <h6>nome antipasto</h6>
-                <div>descrizione antipasto</div>
+        <div className="bodyMenu w-4/5 m-auto text-center bg-[var(--color-honey)] mb-10 p-5">
+            <h1 className="capitalize text-3xl mb-10">il menu propone</h1>
+            <div className="entrè mb-20">
+                <h3 className="capitalize text-2xl mb-10">antipasti</h3>
+                <div className="grid grid-cols-2 gap-y-8">
+                    {antipasti?.length !== 0 ? antipasti.filter(antipasto => antipasto.tipologia !== 'Mare').map(antipasto => (
+                        <div className="p-2 flex flex-col content-between" key={antipasto?.id}>
+                            <h6 className="mb-3">{antipasto?.nome_piatto}</h6>
+                            <div className="mb-3" >{antipasto?.descrizione_piatto}</div>
+                            <div className="w-3/5 m-auto flex justify-evenly mb-3 pb-6 border-b-2 border-double">
+                                {antipasto.glutenfree ? <GiFlour className="text-green-700" /> : <GiFlour className="text-red-700" />}
+                                {antipasto.vegano ? <GiLindenLeaf className="text-green-700" /> : <GiLindenLeaf className="text-red-700" />}
+                            </div>
+                        </div>
+                    )) : <h2>no antipasti</h2>}
+                    {antipasti?.length !== 0 ? antipasti.filter(antipasto => antipasto.tipologia === 'Mare').map(antipasto => (
+                        <div className="p-2 flex flex-col content-between" key={antipasto?.id}>
+                            <h6 className="mb-3">{antipasto?.nome_piatto}</h6>
+                            <div className="mb-3" >{antipasto?.descrizione_piatto}</div>
+                            <div className="w-3/5 m-auto flex justify-evenly mb-3 pb-6 border-b-2 border-double">
+                                {antipasto.glutenfree ? <GiFlour className="text-green-700" /> : <GiFlour className="text-red-700" />}
+                                {antipasto.vegano ? <GiLindenLeaf className="text-green-700" /> : <GiLindenLeaf className="text-red-700" />}
+                            </div>
+                        </div>
+                    )) : <h2>no antipasti</h2>}
+
+
+                </div>
             </div>
-            <div className="mainDish">
-                <h3>primi</h3>
-                <h6>nome antipasto</h6>
-                <div>descrizione antipasto</div>
+            <div className="mainDish mb-20">
+                <h3 className="capitalize text-2xl mb-10">primi piatti</h3>
+                <div className="grid grid-cols-2 gap-y-8">
+                    {primi?.length !== 0 ? primi.filter(primo => primo.tipologia !== 'Mare').map(primo => (
+                        <div className="p-2 flex flex-col content-between" key={primo?.id}>
+                            <h6 className="mb-3">{primo?.nome_piatto}</h6>
+                            <div className="mb-3" >{primo?.descrizione_piatto}</div>
+                            <div className="w-3/5 m-auto flex justify-evenly mb-3 pb-6 border-b-2 border-double">
+                                {primo.glutenfree ? <GiFlour className="text-green-700" /> : <GiFlour className="text-red-700" />}
+                                {primo.vegano ? <GiLindenLeaf className="text-green-700" /> : <GiLindenLeaf className="text-red-700" />}
+
+                            </div>
+                        </div>
+                    )) : <h2>no primi</h2>}
+                    {primi?.length !== 0 ? primi.filter(primo => primo.tipologia === 'Mare').map(primo => (
+                        <div className="p-2 flex flex-col content-between" key={primo?.id}>
+                            <h6 className="mb-3">{primo?.nome_piatto}</h6>
+                            <div className="mb-3" >{primo?.descrizione_piatto}</div>
+                            <div className="w-3/5 m-auto flex justify-evenly mb-3 pb-6 border-b-2 border-double">
+                                {primo.glutenfree ? <GiFlour className="text-green-700" /> : <GiFlour className="text-red-700" />}
+                                {primo.vegano ? <GiLindenLeaf className="text-green-700" /> : <GiLindenLeaf className="text-red-700" />}
+
+                            </div>
+                        </div>
+                    )) : <h2>no primi</h2>}
+
+
+                </div>
             </div>
-            <div className="second">
-                <h3>secondi</h3>
-                <h6>nome antipasto</h6>
-                <div>descrizione antipasto</div>
+            <div className="second mb-20">
+                <h3 className="capitalize text-2xl mb-10">secondi piatti</h3>
+                <div className="grid grid-cols-2 gap-y-8">
+                    {secondi?.length !== 0 ? secondi.filter(secondo => secondo.tipologia !== 'Mare').map(secondo => (
+                        <div className="p-2 flex flex-col content-between" key={secondo?.id}>
+                            <h6 className="mb-3">{secondo?.nome_piatto}</h6>
+                            <div className="mb-3" >{secondo?.descrizione_piatto}</div>
+                            <div className="w-3/5 m-auto flex justify-evenly mb-3 pb-6 border-b-2 border-double">
+                                {secondo.glutenfree ? <GiFlour className="text-green-700" /> : <GiFlour className="text-red-700" />}
+                                {secondo.vegano ? <GiLindenLeaf className="text-green-700" /> : <GiLindenLeaf className="text-red-700" />}
+
+                            </div>
+                        </div>
+                    )) : <h2>no secondi</h2>}
+                    {secondi?.length !== 0 ? secondi.filter(secondo => secondo.tipologia === 'Mare').map(secondo => (
+                        <div className="p-2 flex flex-col content-between" key={secondo?.id}>
+                            <h6 className="mb-3">{secondo?.nome_piatto}</h6>
+                            <div className="mb-3" >{secondo?.descrizione_piatto}</div>
+                            <div className="w-3/5 m-auto flex justify-evenly mb-3 pb-6 border-b-2 border-double">
+                                {secondo.glutenfree ? <GiFlour className="text-green-700" /> : <GiFlour className="text-red-700" />}
+                                {secondo.vegano ? <GiLindenLeaf className="text-green-700" /> : <GiLindenLeaf className="text-red-700" />}
+
+                            </div>
+                        </div>
+                    )) : <h2>no secondi</h2>}
+
+
+                </div>
             </div>
-            <div className="dessert">
-                <h3>dessert</h3>
-                <h6>nome antipasto</h6>
-                <div>descrizione antipasto</div>
+            <div className="dessert mb-20">
+                <h3 className="capitalize text-2xl mb-10">desserts</h3>
+                <div className="grid grid-cols-2 gap-y-8">
+                    {desserts?.length !== 0 ? desserts.filter(dessert => dessert.tipologia !== 'Mare').map(dessert => (
+                        <div className="p-2 flex flex-col content-between" key={dessert?.id}>
+                            <h6 className="mb-3">{dessert?.nome_piatto}</h6>
+                            <div className="mb-3" >{dessert?.descrizione_piatto}</div>
+                            <div className="w-3/5 m-auto flex justify-evenly mb-3 pb-6 border-b-2 border-double">
+                                {dessert.glutenfree ? <GiFlour className="text-green-700" /> : <GiFlour className="text-red-700" />}
+                                {dessert.vegano ? <GiLindenLeaf className="text-green-700" /> : <GiLindenLeaf className="text-red-700" />}
+
+                            </div>
+                        </div>
+                    )) : <h2>no desserts</h2>}
+                    {desserts?.length !== 0 ? desserts.filter(dessert => dessert.tipologia === 'Mare').map(dessert => (
+                        <div className="p-2 flex flex-col content-between" key={dessert?.id}>
+                            <h6 className="mb-3">{dessert?.nome_piatto}</h6>
+                            <div className="mb-3" >{dessert?.descrizione_piatto}</div>
+                            <div className="w-3/5 m-auto flex justify-evenly mb-3 pb-6 border-b-2 border-double">
+                                {dessert.glutenfree ? <GiFlour className="text-green-700" /> : <GiFlour className="text-red-700" />}
+                                {dessert.vegano ? <GiLindenLeaf className="text-green-700" /> : <GiLindenLeaf className="text-red-700" />}
+
+                            </div>
+                        </div>
+                    )) : <h2>no desserts</h2>}
+
+
+                </div>
             </div>
+            <div>** Puoi comunicarci qualsiasi intolleranza o modifica sul piatto scelto **</div>
         </div>
     )
 }
